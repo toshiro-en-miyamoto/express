87c0ecd010faea7a4c2abfae5fbaf68897f4232e
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2020-08-09 22:22:57 +0800

    Chapter 3. Saving Time with Express

---
+++ brown/README.md
@@ -0,0 +1,7 @@
+# Meadowlark Travel web app
+* Web Development with Node and Express, 2nd Edition
+* Ethan Brown, November 2019 (ISBN: 9781492053507)
+# Chapter 3. Saving Time with Express
+* $ npm init
+* $ npm install express
+* $ npm install express-handlebars
---
+++ brown/meadowlark.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.render(('home')))
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+  ]
+  
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
+
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => console.log(
+    `Express started on http://127.0.0.1:${port}; ` +
+    `press Ctrl-C to terminate.`
+))
